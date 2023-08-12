30190e6bf93a525db6d7d2c9fd4042fbf5cefd11
Lindsay Warr <iamalion@gmail.com>
2023-08-12 08:54:13 -0700

    Add splash page

--- SweetAndSavory/Controllers/HomeController.cs
+++ SweetAndSavory/Controllers/HomeController.cs
@@ -1,15 +1,42 @@
 using Microsoft.AspNetCore.Mvc;
+using SweetAndSavory.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
 
 namespace SweetAndSavory.Controllers
 {
     public class HomeController : Controller
     {
-      [HttpGet("/")]
-      public ActionResult Index()
-      {
-        ViewBag.PageTitle = "Pierre's Bakery";
-        return View();
-      }
+        private readonly SweetAndSavoryContext _db;
+        private readonly UserManager<ApplicationUser> _userManager;
 
-    }
+        public HomeController(UserManager<ApplicationUser> userManager, SweetAndSavoryContext db)
+        {
+          _userManager = userManager;
+          _db = db;
+        }
+        [HttpGet("/")]
+        public async Task<IActionResult> Index()
+        {
+            ViewBag.PageTitle = "Pierre's Bakery";
+
+            var flavors = _db.Flavors.ToList();
+            var treats = _db.Treats.ToList();
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var user = await _userManager.FindByIdAsync(userId);
+
+            var model = new Dictionary<string, object>
+            {
+                { "flavors", flavors },
+                { "treats", treats },
+                // { "user", user } // Add user to the model if needed
+            };
+
+            return View(model);
+        }
+     }
 }
