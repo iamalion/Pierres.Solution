bea5bb087b891e51840bf28f35ca8a933a2ea72d
Lindsay Warr <iamalion@gmail.com>
2023-08-12 09:49:38 -0700

    Final tweaks

--- SweetAndSavory/Controllers/FlavorsController.cs
+++ SweetAndSavory/Controllers/FlavorsController.cs
@@ -11,7 +11,7 @@ using System.Security.Claims;
 
 namespace SweetAndSavory.Controllers
 {
-    [Authorize]
+   
     public class FlavorsController : Controller
     {
         private readonly SweetAndSavoryContext _db;
